57f6144e82a673e9740b699bdd6ae2707fe3f2b7
Matheus Sanches <rm87426@fiap.com.br>
2021-07-07 11:16:10 -0300

    Menu 2/ terceira serie finalizado

--- CheckPoint3/Escola.py
+++ CheckPoint3/Escola.py
@@ -13,6 +13,22 @@ Op2M2 = []
 Op2V1 = []
 Op2V2 = []
 
+Op3M1 = []
+Op3M2 = []
+Op3M3 = []
+Op3V1 = []
+
+Op4M1 = []
+Op4M2 = []
+Op4M3 = []
+Op4V1 = []
+
+Op5M1 = []
+Op5M2 = []
+Op5M3 = []
+Op5V1 = []
+
+
 def menu():
     print("-"*50)
     print("COLEGIO NOVA ESPERANÇA-EVENTO LITERARIO")
@@ -71,9 +87,9 @@ def studentsSubscribe():
         #oficinas disponiveis para o segundo ano
         if rm in rm2class:
             print("***Oficinas Disponiveis***")
-            print("****MATUTINO****")
             count = 0
             while count < 3:
+                print("****MATUTINO****")
             #verificando disponibilidade de oficinas
                 if len(Op2M1) < 10: 
                     print("--Segunda-Feira-- Criar e contar histórias (Opção--> 1)")
@@ -130,6 +146,7 @@ def studentsSubscribe():
                         count = count - 1
                         opcao = int(input("---> "))
                 elif opcao == 0:
+                    count = count - 1
                     menu()
                 count += 1
                 print(f' Você ja realizou {count} inscrições!')
@@ -138,21 +155,137 @@ def studentsSubscribe():
         #oficinas disponiveis para o terceiro ano
         elif rm in rm3class:
             print("***Oficinas Disponiveis***")
-            print("****MATUTINO****")
-            print("--Segunda-Feira-- Criar e contar histórias")
-            print("--Terça-Feira-- Teatro: Luz, Câmera e Ação")
-            print("--Quarta-Feira-- A lingua dos sinais")
-            print("****VESPERTINO****")
-            print("--Terça-Feira-- O corpo fala")
+            count = 0
+            while count < 3:
+                print("****MATUTINO****")
+                #verificando disponibilidade de oficinas
+                if len(Op3M1) < 10: 
+                    print("--Segunda-Feira-- Criar e contar histórias (Opção--> 1)")
+                    print(f'Já foram realizadas {len(Op3M1)} inscrições!')
+                    #quantas inscriçoes ja foram realizadas
+                if len(Op3M2) < 10:
+                    print("--Terça-Feira-- Teatro: Luz, Câmera e Ação (Opção--> 2)")
+                    print(f'Já foram realizadas {len(Op3M2)} inscrições!')
+                if len(Op3M3) < 10:
+                    print("--Quarta-Feira-- A lingua dos sinais (Opção--> 3)")
+                    print(f'Já foram realizadas {len(Op3M3)} inscrições!')
+                print("****VESPERTINO****")
+                if len(Op3V1) < 10:
+                    print("--Terça-Feira-- O corpo fala (Opção--> 4)")
+                    print(f'Já foram realizadas {len(Op3V1)} inscrições!')
+                print("Retorno ao menu --> 0")
+                #solicitando que o usuario escolha uma opção
+                opcao = int(input("Escolha uma opção --> "))
+                #validando para que seja uma opção existente 
+                while opcao < 0 or opcao > 4:
+                    print("Opção invalida!")
+                    opcao = int(input("Digite uma opção valida: "))
+                if opcao == 1:
+                    #verificando se o usuario ja esta cadastrado em tal oficina, se nao, cadastrar.
+                    if rm not in Op3M1:
+                        Op3M1.append(rm)
+                        menu()
+                    else: 
+                        print("Opção já cadastrada, digite outra opção")
+                        count = count - 1
+                        opcao = int(input("---> "))
+                elif opcao == 2:
+                    if rm not in Op3M2:
+                        Op3M2.append(rm)
+                        menu()
+                    else:
+                        print("Opção já cadastrada, digite outra opção")
+                        count = count - 1
+                        opcao = int(input("---> "))
+                elif opcao == 3:
+                    if rm not in Op3M3:
+                        Op3M3.append(rm)
+                        menu()
+                    else:
+                        print("Opção já cadastrada, digite outra opção")
+                        count = count - 1
+                        opcao = int(input("---> "))
+                elif opcao == 4:
+                    if rm not in Op3V1:
+                        Op3V1.append(rm)
+                        menu()
+                    else:
+                        print("Opção já cadastrada, digite outra opção")
+                        count = count - 1
+                        opcao = int(input("---> "))
+                elif opcao == 0:
+                    count = count - 1
+                    menu()
+                count += 1
+                print(f' Você ja realizou {count} inscrições!')
+                
         #oficinas disponiveis para o quarto ano
         elif rm in rm4class:
             print("***Oficinas Disponiveis***")
-            print("****MATUTINO****")
-            print("--Terça-Feira-- Teatro: Luz, Câmera e Ação")
-            print("--Quarta-Feira-- A lingua dos sinais")
-            print("--Quinta-Feira-- Expressão Artística")
-            print("****VESPERTINO****")
-            print("--Segunda-Feira-- Leitura dramática")
+            count = 0
+            while count < 3:
+                print("****MATUTINO****")
+                #verificando disponibilidade de oficinas
+                if len(Op3M1) < 10: 
+                    print("--Segunda-Feira-- Criar e contar histórias (Opção--> 1)")
+                    print(f'Já foram realizadas {len(Op3M1)} inscrições!')
+                    #quantas inscriçoes ja foram realizadas
+                if len(Op3M2) < 10:
+                    print("--Terça-Feira-- Teatro: Luz, Câmera e Ação (Opção--> 2)")
+                    print(f'Já foram realizadas {len(Op3M2)} inscrições!')
+                if len(Op3M3) < 10:
+                    print("--Quarta-Feira-- A lingua dos sinais (Opção--> 3)")
+                    print(f'Já foram realizadas {len(Op3M3)} inscrições!')
+                print("****VESPERTINO****")
+                if len(Op3V1) < 10:
+                    print("--Terça-Feira-- O corpo fala (Opção--> 4)")
+                    print(f'Já foram realizadas {len(Op3V1)} inscrições!')
+                print("Retorno ao menu --> 0")
+                #solicitando que o usuario escolha uma opção
+                opcao = int(input("Escolha uma opção --> "))
+                #validando para que seja uma opção existente 
+                while opcao < 0 or opcao > 4:
+                    print("Opção invalida!")
+                    opcao = int(input("Digite uma opção valida: "))
+                if opcao == 1:
+                    #verificando se o usuario ja esta cadastrado em tal oficina, se nao, cadastrar.
+                    if rm not in Op3M1:
+                        Op3M1.append(rm)
+                        menu()
+                    else: 
+                        print("Opção já cadastrada, digite outra opção")
+                        count = count - 1
+                        opcao = int(input("---> "))
+                elif opcao == 2:
+                    if rm not in Op3M2:
+                        Op3M2.append(rm)
+                        menu()
+                    else:
+                        print("Opção já cadastrada, digite outra opção")
+                        count = count - 1
+                        opcao = int(input("---> "))
+                elif opcao == 3:
+                    if rm not in Op3M3:
+                        Op3M3.append(rm)
+                        menu()
+                    else:
+                        print("Opção já cadastrada, digite outra opção")
+                        count = count - 1
+                        opcao = int(input("---> "))
+                elif opcao == 4:
+                    if rm not in Op3V1:
+                        Op3V1.append(rm)
+                        menu()
+                    else:
+                        print("Opção já cadastrada, digite outra opção")
+                        count = count - 1
+                        opcao = int(input("---> "))
+                elif opcao == 0:
+                    count = count - 1
+                    menu()
+                count += 1
+                print(f' Você ja realizou {count} inscrições!')
+            
         #oficinas disponiveis para o quinto ano 
         elif rm in rm5class:
             print("***Oficinas Disponiveis***")
@@ -165,7 +298,11 @@ def studentsSubscribe():
     else:
         print("Aluno não cadastrado. Favor procurar a coordenação do Fundamental I")
 
-
+def listRegistration():
+    print("")
+    print("-"*50)
+    print("LISTA DE INSCRIÇÕES")
+    print("-"*50)
         
 while True:
     option = menu()
@@ -177,7 +314,7 @@ while True:
     elif option == 2:
         studentsSubscribe()
     elif option == 3:
-        print("Em construção, aguarde nova atualização!")
+        listRegistration()
     elif option == 4:
         break
         
